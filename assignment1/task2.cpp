c050ab910122456661f8bc99752a9145d47b50e9
Hady <hadyjanuar95@gmail.com>
2019-02-18 06:31:20 -0800

    Move files for clarity, update printing of basic blocks to be singular among task2 items

--- assignment1/task2.cpp
+++ assignment1/task2.cpp
@@ -16,6 +16,9 @@
 using namespace llvm;
 
 void generatePath(BasicBlock *BB);
+void traverse(BasicBlock *BB);
+void print(const BasicBlock *bb);
+std::string getSimpleNodeLabel(const BasicBlock *Node);
 
 int main(int argc, char **argv)
 {
@@ -30,13 +33,47 @@ int main(int argc, char **argv)
     }
 
     for (auto &F : *M)
+    {
         if (strncmp(F.getName().str().c_str(), "main", 4) == 0)
         {
             BasicBlock *BB = dyn_cast<BasicBlock>(F.begin());
-            BB->print(llvm::errs(), true);
-            generatePath(BB);
+            print(BB);
+            traverse(BB);
         }
+    }
+
     return 0;
+}
+
+void print(const BasicBlock *bb)
+{
+    llvm::outs() << "Label:" << getSimpleNodeLabel(bb) << "\n";
+    // bb->print(llvm::errs(), false);
+}
+
+
+// Printing Basic Block Label
+std::string getSimpleNodeLabel(const BasicBlock *Node)
+{
+    if (!Node->getName().empty())
+        return Node->getName().str();
+    std::string Str;
+    raw_string_ostream OS(Str);
+    Node->printAsOperand(OS, false);
+    return OS.str();
+}
+
+void traverse(BasicBlock *BB)
+{
+    const TerminatorInst *TInst = BB->getTerminator();
+    unsigned NSucc = TInst->getNumSuccessors();
+
+    for (unsigned i = 0; i < NSucc; i++)
+    {
+        BasicBlock *Succ = TInst->getSuccessor(i);
+        print(Succ);
+        traverse(Succ);
+    }
 }
 
 void generatePath(BasicBlock *BB)
@@ -46,7 +83,7 @@ void generatePath(BasicBlock *BB)
     if (NSucc == 1)
     {
         BasicBlock *Succ = TInst->getSuccessor(0);
-        Succ->print(llvm::errs(), true);
+        print(Succ);
         generatePath(Succ);
     }
     else if (NSucc > 1)
@@ -54,7 +91,7 @@ void generatePath(BasicBlock *BB)
         srand(time(NULL));
         unsigned rnd = std::rand() / (RAND_MAX / NSucc); // rand() return a number between 0 and RAND_MAX
         BasicBlock *Succ = TInst->getSuccessor(rnd);
-        Succ->print(llvm::errs(), true);
+        print(Succ);
         generatePath(Succ);
     }
 }
