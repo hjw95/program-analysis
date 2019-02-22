988c200b0de82b75138b3a3a5a1917c8f38f02ee
Hady <hadyjanuar95@gmail.com>
2019-02-22 03:30:10 -0800

    Combine stack from parent, test against loop

--- assignment1/task2.cpp
+++ assignment1/task2.cpp
@@ -19,7 +19,7 @@
 using namespace llvm;
 using namespace std;
 
-void traverse(BasicBlock *BB);
+void traverse(BasicBlock *BB, set<Instruction *> parentSet);
 void print(const Value *bb);
 void print(const Instruction *bb);
 void print(const BasicBlock *bb);
@@ -30,14 +30,13 @@ string getSimpleNodeLabel(const Instruction *Node);
 
 map<string, set<Instruction *>> analysisMap;
 
-map<string, set<string>> initializedVariables;
-
 int main(int argc, char **argv)
 {
     // Read the IR file.
     static LLVMContext Context;
     SMDiagnostic Err;
     unique_ptr<Module> M = parseIRFile(argv[1], Err, Context);
+
     if (M == nullptr)
     {
         fprintf(stderr, "error: failed to load LLVM IR file \"%s\"", argv[1]);
@@ -50,7 +49,8 @@ int main(int argc, char **argv)
         {
             BasicBlock *BB = dyn_cast<BasicBlock>(F.begin());
             print(BB);
-            traverse(BB);
+            set<Instruction *> emptySet;
+            traverse(BB, emptySet);
         }
     }
 
@@ -60,74 +60,53 @@ int main(int argc, char **argv)
     return 0;
 }
 
-string getSimpleNodeLabel(const BasicBlock *Node)
-{
-    if (!Node->getName().empty())
-        return Node->getName().str();
-
-    string Str;
-    raw_string_ostream OS(Str);
-    Node->printAsOperand(OS, false);
-    return OS.str();
-}
-
-string getSimpleNodeLabel(const Instruction *Node)
-{
-    if (!Node->getName().empty())
-        return Node->getName().str();
-
-    string Str;
-    raw_string_ostream OS(Str);
-    Node->printAsOperand(OS, false);
-    return OS.str();
-}
-
-void traverse(BasicBlock *BB)
+void traverse(BasicBlock *BB, set<Instruction *> parentSet)
 {
     const TerminatorInst *TInst = BB->getTerminator();
     unsigned NSucc = TInst->getNumSuccessors();
 
     set<Instruction *> affectingInstruction;
-    set<string> initializedVariables;
 
     unsigned originalCount = 0;
     bool traversed = false;
 
+    for (auto &I : parentSet)
+    {
+        affectingInstruction.insert(I);
+    }
+
     if (analysisMap.count(getSimpleNodeLabel(BB)) > 0)
     {
-        // found
-        affectingInstruction = analysisMap[getSimpleNodeLabel(BB)];
-        originalCount = affectingInstruction.size();
+        for (auto &I : analysisMap[getSimpleNodeLabel(BB)])
+        {
+            affectingInstruction.insert(I);
+        }
+        originalCount = analysisMap[getSimpleNodeLabel(BB)].size();
         traversed = true;
     }
 
     for (auto &I : *BB)
     {
-        if (isa<LoadInst>(I))
-        {
-            Value *v = I.getOperand(0);
-            Instruction *var = dyn_cast<Instruction>(v);
-            affectingInstruction.insert(var);
-        }
-        else if (isa<StoreInst>(I))
+        if (isa<StoreInst>(I))
         {
             Value *v = I.getOperand(1);
             Instruction *var = dyn_cast<Instruction>(v);
             affectingInstruction.insert(var);
         }
-        else
-        {
-            outs() << "Skipped ";
-            print(&I);
-        }
     }
 
     analysisMap[getSimpleNodeLabel(BB)] = affectingInstruction;
+
+    if (NSucc == 0)
+    {
+        return;
+    }
 
     unsigned finalCount = affectingInstruction.size();
 
     if (traversed && originalCount == finalCount)
     {
+        cout << "This shouldn't happen yet\n";
         return;
     }
 
@@ -135,7 +114,7 @@ void traverse(BasicBlock *BB)
     {
         BasicBlock *Succ = TInst->getSuccessor(i);
         print(Succ);
-        traverse(Succ);
+        traverse(Succ, affectingInstruction);
     }
 }
 
@@ -175,4 +154,26 @@ void print(const Value *bb)
     // outs() << "Label:" << getSimpleNodeLabel(bb) << "\n";
     bb->print(llvm::errs(), false);
     outs() << "\n";
+}
+
+string getSimpleNodeLabel(const BasicBlock *Node)
+{
+    if (!Node->getName().empty())
+        return Node->getName().str();
+
+    string Str;
+    raw_string_ostream OS(Str);
+    Node->printAsOperand(OS, false);
+    return OS.str();
+}
+
+string getSimpleNodeLabel(const Instruction *Node)
+{
+    if (!Node->getName().empty())
+        return Node->getName().str();
+
+    string Str;
+    raw_string_ostream OS(Str);
+    Node->printAsOperand(OS, false);
+    return OS.str();
 }
