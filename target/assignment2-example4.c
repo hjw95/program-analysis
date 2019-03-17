956e0847c03642b07f34e5b81c472b958d6af7bb
Hady <hadyjanuar95@gmail.com>
2019-03-17 15:09:57 +0800

    Add sink and sources

---
+++ target/assignment2-example4.c
@@ -0,0 +1,20 @@
+int main()
+{
+    int b, c, d, sink, source, N;
+    // read source from input
+    int i = 0;
+    while (i < N)
+    {
+        if (i % 2 == 0)
+        {
+            b = source;
+            d = source + 1;
+        }
+        else
+        {
+            c = b;
+        }
+        i++;
+    }
+    sink = c;
+}
